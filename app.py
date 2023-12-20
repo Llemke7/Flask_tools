d33d2cd7f293e3bdbfad8b2d1804314e3d7b3d4a
Luke Lemke <llemke777@gmail.com>
2023-12-20 16:54:24 -0600

    sessions

--- app.py
+++ app.py
@@ -1,36 +1,41 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
 app = Flask(__name__,)
-app.config['SECRET_KEY']= 'super_secret_key'
+app.config['SECRET_KEY']= 'secretkey'
 
-responses = ['Yes', 'No', 'Less than $10,000', 'Yes']
+responses = []
 
 @app.route('/')
 def home():
-    return render_template('base.html', survey= satisfaction_survey)
+    return render_template('base.html', survey= satisfaction_survey, display=False)
 
 @app.route('/questions/<int:question_id>', methods=['GET', 'POST'])
 def question(question_id):
     try:
         if question_id >= len(satisfaction_survey.questions):
             return "Survey or question not found!"
+
         question = satisfaction_survey.questions[question_id]
         if request.method == 'POST':
             answer = request.form.get('choice')
-            responses.append({question_id: answer})
-            
+            session_responses = session.get('responses', [])
+            session_responses.append({question_id: answer})
+
+            session['responses'] = session_responses
+
             if question_id + 1 < len(satisfaction_survey.questions):
                 return redirect(f'/questions/{question_id + 1}')
             else:
                 return render_template('thank_you.html')
+
         return render_template('question.html', survey=satisfaction_survey, question=question, question_id=question_id)
+        
     except Exception as e:
         print(f"Error: {str(e)}")
-        return "Error ocurred"
+        return "Error occurred"
 
 @app.route('/thank_you')
 def thank_you():
     return render_template('thank_you.html')
-
